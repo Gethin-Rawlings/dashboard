ccdf867779aa85144467e63971380b2f78170154
Rawlings <GRW@traverssmith.com>
2019-07-22 14:46:03 +0100

    Chart loading data from SQL

--- src/App.js
+++ src/App.js
@@ -4,63 +4,52 @@ import FusionCharts from "fusioncharts";
 import Charts from "fusioncharts/fusioncharts.charts";
 import ReactFC from "react-fusioncharts";
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
-
+import { getChartData }  from './apiCalls'
 
 ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
-const dataSource = {
-  chart: {
-    caption: "Calls opened today",
-   // subcaption: "In MMbbl = One Million barrels",
-    // xaxisname: "Country",
-    // yaxisname: "Reserves (MMbbl)",
-    // numbersuffix: "K",
-    theme: "candy"
-  },
-  data: [
-    {
-      label: "07",
-      value: "10"
-    },
-    {
-      label: "08",
-      value: "30"
-    },
-    {
-      label: "09",
-      value: "50"
-    },
-    {
-      label: "10",
-      value: "10"
-    },
-    {
-      label: "11",
-      value: "15"
-    },
-    {
-      label: "12",
-      value: "24"
-    },
-    {
-      label: "13",
-      value: "30"
-    },
-    {
-      label: "14",
-      value: "30"
-    }
-  ]
-};
-
 class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        callsOpened: [],
+        requestFailed: false,
+        dataSource : {
+          chart: {
+            caption: "Calls opened today",
+            theme: "candy"
+          }
+        }
+      }
+    }
+    
+  async  componentWillMount() {
+    try {
+      const url = 'http://localhost:5000/customWidgetData';
+      const data = await getChartData(url)
+      this.setState({callsOpened: data})
+      console.log(JSON.stringify(`whats going on ${this.state.callsOpened}`))
+    } catch (error) {
+      this.setState({ requestFailed: true })
+    }
+  };
+
   render() {
+    if (this.state.requestFailed) return <p>Failed!</p>
+    let dataSource = this.state.dataSource
+    let callsOpened = this.state.callsOpened
+    let newDataSource = {...dataSource, data : callsOpened}
+    console.log(`calls opened  ${JSON.stringify(callsOpened)}`);
+
     return (
-      <ReactFC
+      <div className = 'Chart 1'>
+        <ReactFC
         type="column2d"
         dataFormat="JSON"
-        dataSource={dataSource}
+        dataSource={newDataSource}
       />
+      </div>
+      
     );
   }
 }
---
+++ src/apiCalls.js
@@ -0,0 +1,9 @@
+
+export const getChartData = async(url) => {
+    const response = await fetch(url)
+    if (Response.status >= 400) {
+        throw( new Error('Network Request Failed'))
+    } else {
+        return await response.json()
+    }
+};
