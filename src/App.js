9678d8fc91a31c98579fabe8df7bd0ba31428987
Gethin Rawlings <gethin.p.rawlings@gmail.com>
2019-07-22 10:17:31 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const dataSource = {
+  chart: {
+    caption: "Calls opened today",
+   // subcaption: "In MMbbl = One Million barrels",
+    // xaxisname: "Country",
+    // yaxisname: "Reserves (MMbbl)",
+    // numbersuffix: "K",
+    theme: "candy"
+  },
+  data: [
+    {
+      label: "07",
+      value: "10"
+    },
+    {
+      label: "08",
+      value: "30"
+    },
+    {
+      label: "09",
+      value: "50"
+    },
+    {
+      label: "10",
+      value: "10"
+    },
+    {
+      label: "11",
+      value: "15"
+    },
+    {
+      label: "12",
+      value: "24"
+    },
+    {
+      label: "13",
+      value: "30"
+    },
+    {
+      label: "14",
+      value: "30"
+    }
+  ]
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <ReactFC
+        type="column2d"
+        dataFormat="JSON"
+        dataSource={dataSource}
+      />
+    );
+  }
 }
 
 export default App;
